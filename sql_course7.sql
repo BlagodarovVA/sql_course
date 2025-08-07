c9a1460ee53f10e3e267f18e5b4bebd932f25840
Valery Blagodarov <11w-walerik@mail.ru>
2025-08-07 16:47:20 +0300

    146 done

--- sql_course7.sql
+++ sql_course7.sql
@@ -1,4 +1,4 @@
--- 144
+-- 144 VIEW
 select * from employees;
 
 create view fin_emp AS
@@ -34,7 +34,7 @@ CREATE TABLE students (
 INSERT INTO students VALUES ( 1, 'Diana', 3, 'diana@doc.com', 1 );
 INSERT INTO students VALUES ( 3, 'Valery', 4, 'valery@it.com', 2 );
 
--- 145
+-- 145 Simple и Complex
 select * from students;
 
 create view v101 as select name, course from students;
@@ -60,6 +60,53 @@ insert into v107 values ('Zinaida', 2);
 
 create view v108 as select rownum r, name, course from students;
 select * from v108;
+
+-- 146 CREATE VIEW
+select * from students;
+drop TABLE students;
+
+CREATE TABLE students (
+   id         NUMBER,
+   name       VARCHAR2(15),
+   course     NUMBER
+);
+
+INSERT INTO students VALUES ( 1, 'Diana', 3);
+INSERT INTO students VALUES ( 3, 'Valery', 4);
+INSERT INTO students VALUES ( 2, 'Frosya', 2);
+INSERT INTO students VALUES ( 4, 'Hamyak', 1);
+
+create or replace VIEW fin_emp2 AS select * from students;
+select * from fin_emp2;
+
+create FORCE VIEW v201 AS select * from students111;
+
+create FORCE VIEW v202 AS select * from students where course > 2;
+select * from v202;
+INSERT INTO v202 VALUES ( 5, 'Katya', 2);
+
+create FORCE VIEW v203 AS select * from students where course > 2 WITH CHECK OPTION;
+select * from v203;
+INSERT INTO v203 VALUES ( 6, 'Ivan', 1);    -- не прокатит, т.к. чек не проходит
+update v203 set course = 2 where name = 'Diana'; -- не прокатит, т.к. чек не проходит
+
+create VIEW v204 AS select * from students WITH READ ONLY;
+select * from v204;
+update v204 set course = 5 where name = 'Ivan'; -- не прокатит, т.к. вьюха для чтения
+
+create VIEW v205 (uniq_id, st_name, st_course) AS select * from students;
+select * from v205;
+
+-- 147 ALTER и DROP
+
+
+
+
+
+
+
+
+
 
 
 
